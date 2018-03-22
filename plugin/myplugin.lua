109ae1e3bd7fb9a9f17af4a6899cf4bff4c0fc5a
develephant <code@develephant.com>
2018-03-21 19:23:51 -0500

    build

--- plugin/myplugin.lua
+++ plugin/myplugin.lua
@@ -9,8 +9,16 @@ local platform = system.getInfo("platform")
 if platform == 'html5' then
   lib = require("myplugin_js")
 else
+	-- wrapper for non web platforms
+	local CoronaLibrary = require "CoronaLibrary"
+	-- Create stub library for simulator
+	lib = CoronaLibrary:new{ name='myplugin', publisherId='com.me' }
   -- Alert for non-HTML5 platforms
-  error( "The '" .. lib.name .. "' library is not available on this platform." )
+	local function defaultFunction()
+		print( "WARNING: The '" .. lib.name .. "' library is not available on this platform." )
+  end
+  
+  lib.echo = defaultFunction
 end
 
 return lib
